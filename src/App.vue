1ab89f269e69cde47c46d19be5a98a21775c20b3
danielhinsching <danielsantoshinsching@gmail.com>
2023-04-03 11:57:22 -0300

    lançamento 3 abril

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,64 @@
 <script setup>
 import { ref } from 'vue'
 const nome = ref('')
-const texto = ref('')
-const aceita = ref(false)
+const email = ref('')
+const senha = ref('')
+const confirmaSenha = ref('')
+const data = ref('')
+const endereço = ref('')
+const cidade = ref('')
+const estado = ref('')
+const hobbies = ref('')
+const linguagemProg = ref('')
+const biografia = ref('')
+
+const aceitar = ref(false)
+
+function enviar() {
+  aceitar.value = !aceitar.value
+  nome.value = ''
+  idade.value = ''
+  email.value = ''
+}
 </script>
 
 <template>
-    <div class="info">
+    <form class="info" @submit.prevent="enviar()">
       <p>Nome:</p>
-      <input type="text" v-model="nome"/>
-      <p>idade:</p>
-      <input type="number" v-model="idade" max="2"/>
+      <input type="text" v-model="nome" minlength="3" maxlength="20"/>
       <p>email:</p>
       <input type="email" v-model="email"/>
-      <p>.</p>
-      <button>Enviar</button>
-      <p>aceita os termos:</p>
-      <label for="aceita">Aceito os termos</label>
+      <p>senha:</p>
+      <input type="password" v-model="senha">
+      <p>confirmar senha:</p>
+      <input type="password" v-model="confirmaSenha">
+      <p>data de nascimento:</p>
+      <input type="date" v-model="data">
+      <p>endereço:</p>
+      <input type="text" v-model="endereço">
+      <P>cidade:</P>
+      <input type="string" v-model="cidade">
+      <p>estado:</p>
+      <input type="string" v-model="estado">
+      <p>hobbies:</p>
+      <input type="text" v-model="hobbies">
+      <p>linguagem de Programação:</p>
+      <p>biografia:</p>
+
+
+
+
+
+      <hr>
+      <button type="submit">Enviar</button>
+      <div v-if="aceitar">
+        <p>{{ nome }}</p> 
+        <p>{{ idade }}</p>
+        <p>{{ email }}</p>
       
-    </div>
+      </div>
+    </form>
+  
 </template>
 
 
